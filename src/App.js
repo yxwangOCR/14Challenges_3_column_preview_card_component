71277fdd450e98ab5f75402e5f3532e3b99b5b64
Yixuan WANG <wang.yixuan@live.fr>
2023-01-10 11:50:22 -0500

    Styled Card

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Card from "./components/Card";
 import IconOne from "../src/images/icon-sedans.svg";
 import IconTwo from "../src/images/icon-suvs.svg";
 import IconThree from "../src/images/icon-luxury.svg";
+import { ThemeProvider } from "styled-components";
 
 function App() {
   const Main = styled.main`
-    background-color: red;
     height: 100vh;
     display: flex;
     flex-direction: column;
@@ -16,29 +16,42 @@ function App() {
     margin: 1em;
   `;
 
+  const StyledCard = styled(Card)`
+    background-color: ${(props) => props.theme.backgroundColor};
+  `;
+
+  const theme = {
+    backgroundColor: "",
+  };
+
   return (
     <Main>
-      <Card
-        URL={IconOne}
-        cardIcon='SEDANS Car Icon'
-        title='SEDANS'
-        text='Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
+      <ThemeProvider theme={theme}>
+        <StyledCard
+          theme={{ backgroundColor: "blue" }}
+          URL={IconOne}
+          cardIcon='SEDANS Car Icon'
+          title='SEDANS'
+          text='Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
   or on your next road trip.'
-      />
-      <Card
-        URL={IconTwo}
-        cardIcon='SUVs Car icon'
-        title='SUVs'
-        text='Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
+        />
+        <StyledCard
+          theme={{ backgroundColor: "green" }}
+          URL={IconTwo}
+          cardIcon='SUVs Car icon'
+          title='SUVs'
+          text='Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
   and off-road adventures.'
-      />
-      <Card
-        URL={IconThree}
-        cardIcon='Luxury Car icon'
-        title='Luxury'
-        text='Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
+        />
+        <StyledCard
+          theme={{ backgroundColor: "yellow" }}
+          URL={IconThree}
+          cardIcon='Luxury Car icon'
+          title='Luxury'
+          text='Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
   rental and arrive in style.'
-      />
+        />
+      </ThemeProvider>
     </Main>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,6 @@
 import Button from "./Button";
 import styled from "styled-components";
 
-const StyledCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 1em 2em;
-  color: #f2f2f2;
-  gap: 2em;
-`;
-
 const Image = styled.img`
   width: 70px;
   height: 50px;
@@ -25,13 +17,24 @@ const Paragraph = styled.p`
 
 const Card = ({ URL, cardIcon, title, text }) => {
   return (
-    <StyledCard>
+    <Card>
       <Image src={URL} alt={cardIcon} />
       <Title>{title}</Title>
       <Paragraph>{text}</Paragraph>
       <Button />
-    </StyledCard>
+    </Card>
   );
 };
 
 export default Card;
+
+/*
+  const StyledCard = styled.div`
+      display: flex;
+      flex-direction: column;
+      padding: 1em 2em;
+      color: #f2f2f2;
+      gap: 2em;
+      background-color: ${(props) => props.theme.backgroundColor};
+    `;
+*/
